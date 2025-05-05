557c574524a4ec4ab9795b98cfd5b74eb5c4e90e
GitFASTBOOT <ahmedshahinx0x@gmail.com>
2025-05-05 23:18:03 +0300

    vicky: load modules by TW target instead of init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,3 +171,4 @@ TW_DEVICE_VERSION := GitFASTBOOT
 # Modules
 TW_LOAD_VENDOR_BOOT_MODULES := true
 TW_MOTOROLA_TOUCH_RMMOD := true
+TW_LOAD_VENDOR_MODULES := "goodix_brl_mmi.ko gt9896s.ko mmi_relay.ko touchscreen_mmi.ko gt9886.ko mmi_info.ko  sensors_class.ko"
